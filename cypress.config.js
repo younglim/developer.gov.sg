175bf2ecd6d3fd708eb91f84612ae9c38b263d89
Chai Pin Zheng <chaipinzheng@gmail.com>
2022-10-26 16:59:51 +0800

    Migrating cypress to v10.0

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  projectId: 'zmz8dj',
+  chromeWebSecurity: false,
+  retries: {
+    runMode: 5,
+    openMode: 5,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8000/',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+}
